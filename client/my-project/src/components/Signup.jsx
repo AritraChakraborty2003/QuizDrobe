1a994b7639fa459361cad51c97b890906fe0ea5d
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-06-17 22:08:49 +0530

    update: update in Users Model and developed GET requet API for score

--- client/my-project/src/components/Signup.jsx
+++ client/my-project/src/components/Signup.jsx
@@ -69,7 +69,7 @@ const Signup = () => {
 
   return (
     <>
-      <div className="LargeDisplayArea hidden lg:block h-screen pb-3">
+      <div className="LargeDisplayArea hidden lg:block ">
         <div className="flex justify-center pb-2">
           <div className="flex justify-center items-center mt-[2vh] bg-backQuiz rounded-[3vmin] w-[98vw]">
             <div className="logoPart w-[44vw] h-[96vh]   font-roboto flex flex-col justify-center items-center">
@@ -199,7 +199,7 @@ const Signup = () => {
       </div>
 
       <div className="block lg:hidden">
-        <div className="md:h-[100vh] bg-gradient-to-bl from-[#9ed8ff] to-backQuiz  lg:bg-gradient-to-tr lg:from-background lg:to-background font-roboto main w-[100vw] flex justify-center items-center">
+        <div className="h-screen bg-gradient-to-bl from-[#9ed8ff] to-backQuiz  lg:bg-gradient-to-tr lg:from-background lg:to-background font-roboto main w-[100vw] flex justify-center items-center">
           <div className="flex flex-col justify-center items-center mt-1">
             <div className="mt-4 from-background to-backQuiz   lg:bg-gradient-to-tl lg:from-background lg:to-background font-roboto box  md:mt-4 h-[42vmin] md:h-[30vmin] md:w-[65vmin] w-[85vw] flex flex-col justify-center items-center p-2">
               <img
