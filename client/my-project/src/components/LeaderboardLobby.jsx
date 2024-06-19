8b205c6a101b3e6acd2aaba44e0870b2b31c793a
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-06-19 19:46:30 +0530

    update: design logical navigation in leaderboard across rounds

--- client/my-project/src/components/LeaderboardLobby.jsx
+++ client/my-project/src/components/LeaderboardLobby.jsx
@@ -21,109 +21,215 @@ const LeaderboardLobby = () => {
         >
           Back
         </button>
-        <div className="mainDisplay bg-backQuiz  lg:h-full  w-[100vw] flex flex-col gap-4 items-center pb-2">
-          <div className="textHeader flex gap-x-3  h-[15vmin] justify-center items-center">
-            <p className="text-2xl lg:text-6xl pb-3 font-extrabold font-roboto mt-5 text-bodytext">
-              <span className="text-[#000]"> Final</span> Leaderboard
-            </p>
-            <div className="holder">
-              <div className="imgHolder flex h-[15vmin] w-[30vmin] items-center ">
-                <img
-                  src="./VDIcon1.png"
-                  alt=""
-                  className="max-w-full max-h-full object-contain"
-                />
+        {(screen.width >= 360 && screen.width < 413 && screen.height > 600 && (
+          <div className="mainDisplay bg-backQuiz  h-[100vh]  w-[100vw] flex flex-col gap-4 items-center pb-2">
+            <div className="textHeader flex gap-x-3  h-[15vmin] justify-center items-center">
+              <p className="text-2xl lg:text-6xl pb-3 font-extrabold font-roboto mt-5 text-bodytext">
+                Leaderboard
+              </p>
+              <div className="holder">
+                <div className="imgHolder flex h-[15vmin] w-[30vmin] items-center ">
+                  <img
+                    src="./VDIcon1.png"
+                    alt=""
+                    className="max-w-full max-h-full object-contain"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="choiceArea w-[100vw] pb-2 flex flex-wrap justify-center items-center text-[5vmin] lg:text-[4.35vmin]  gap-x-7  lg:gap-x-12 2xl:gap-x-[8vmin] font-roboto">
-            <div
-              className="holder pb-2"
-              onClick={() => {
-                setChoice(1);
-              }}
-            >
-              Round 1
-              {(choice === 1 && (
-                <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )) || (
-                <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )}
+            <div className="choiceArea w-[100vw] pb-2 flex flex-wrap justify-center items-center text-[5vmin] lg:text-[4.35vmin]  gap-x-7  lg:gap-x-12 2xl:gap-x-[8vmin] font-roboto">
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(1);
+                }}
+              >
+                Round 1
+                {(choice === 1 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(2);
+                }}
+              >
+                Round 2
+                {(choice === 2 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(3);
+                }}
+              >
+                Round 3
+                {(choice === 3 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(4);
+                }}
+              >
+                Round 4{" "}
+                {(choice === 4 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(5);
+                }}
+              >
+                Round 5{" "}
+                {(choice === 5 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(6);
+                }}
+              >
+                Overall{" "}
+                {(choice === 6 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
             </div>
-            <div
-              className="holder pb-2"
-              onClick={() => {
-                setChoice(2);
-              }}
-            >
-              Round 2
-              {(choice === 2 && (
-                <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )) || (
-                <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )}
-            </div>
-            <div
-              className="holder pb-2"
-              onClick={() => {
-                setChoice(3);
-              }}
-            >
-              Round 3
-              {(choice === 3 && (
-                <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )) || (
-                <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )}
-            </div>
-            <div
-              className="holder pb-2"
-              onClick={() => {
-                setChoice(4);
-              }}
-            >
-              Round 4{" "}
-              {(choice === 4 && (
-                <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )) || (
-                <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )}
-            </div>
-            <div
-              className="holder pb-2"
-              onClick={() => {
-                setChoice(5);
-              }}
-            >
-              Round 5{" "}
-              {(choice === 5 && (
-                <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )) || (
-                <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )}
-            </div>
-            <div
-              className="holder pb-2"
-              onClick={() => {
-                setChoice(6);
-              }}
-            >
-              Overall{" "}
-              {(choice === 6 && (
-                <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )) || (
-                <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
-              )}
-            </div>
-          </div>
 
-          {(choice == 1 && <LeaderboardR1 />) ||
-            (choice === 2 && <LeaderboardR2 />) ||
-            (choice === 3 && <LeaderboardR3 />) ||
-            (choice === 4 && <LeaderboardR4 />) ||
-            (choice === 5 && <LeaderboardR5 />) ||
-            (choice === 6 && <LeaderboardOverall />)}
-        </div>
+            {(choice == 1 && <LeaderboardR1 />) ||
+              (choice === 2 && <LeaderboardR2 />) ||
+              (choice === 3 && <LeaderboardR3 />) ||
+              (choice === 4 && <LeaderboardR4 />) ||
+              (choice === 5 && <LeaderboardR5 />) ||
+              (choice === 6 && <LeaderboardOverall />)}
+          </div>
+        )) || (
+          <div className="mainDisplay bg-backQuiz   w-[100vw] flex flex-col gap-4 items-center pb-2">
+            <div className="textHeader flex gap-x-3  h-[15vmin] justify-center items-center">
+              <p className="text-2xl lg:text-6xl pb-3 font-extrabold font-roboto mt-5 text-bodytext">
+                Leaderboard
+              </p>
+              <div className="holder">
+                <div className="imgHolder flex h-[15vmin] w-[30vmin] items-center ">
+                  <img
+                    src="./VDIcon1.png"
+                    alt=""
+                    className="max-w-full max-h-full object-contain"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="choiceArea w-[100vw] pb-2 flex flex-wrap justify-center items-center text-[5vmin] lg:text-[4.35vmin]  gap-x-7  lg:gap-x-12 2xl:gap-x-[8vmin] font-roboto">
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(1);
+                }}
+              >
+                Round 1
+                {(choice === 1 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(2);
+                }}
+              >
+                Round 2
+                {(choice === 2 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(3);
+                }}
+              >
+                Round 3
+                {(choice === 3 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(4);
+                }}
+              >
+                Round 4{" "}
+                {(choice === 4 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(5);
+                }}
+              >
+                Round 5{" "}
+                {(choice === 5 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+              <div
+                className="holder pb-2"
+                onClick={() => {
+                  setChoice(6);
+                }}
+              >
+                Overall{" "}
+                {(choice === 6 && (
+                  <div className="underline h-5 border-[#000] border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )) || (
+                  <div className="underline h-5 border-bodytext border-t-4 lg:border-t-8 w-30 pt-2"></div>
+                )}
+              </div>
+            </div>
+
+            {(choice == 1 && <LeaderboardR1 />) ||
+              (choice === 2 && <LeaderboardR2 />) ||
+              (choice === 3 && <LeaderboardR3 />) ||
+              (choice === 4 && <LeaderboardR4 />) ||
+              (choice === 5 && <LeaderboardR5 />) ||
+              (choice === 6 && <LeaderboardOverall />)}
+          </div>
+        )}
       </div>
     </>
   );
--- client/my-project/src/components/LeaderboardR1.jsx
+++ client/my-project/src/components/LeaderboardR1.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
 const LeaderboardR1 = () => {
   return (
     <>
